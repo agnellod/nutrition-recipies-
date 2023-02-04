7d93f78bbb8fb07e6fa82d109791b3cfbe634278
cvalerio <cvalerio@csog.net>
2023-02-03 17:38:17 -0700

    bulma

--- Develop/script.js
+++ Develop/script.js
@@ -3,20 +3,20 @@
 //  Maybe a good api to use
 
 
-// const axios = require("axios");
+const axios = require("axios");
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://edamam-recipe-search.p.rapidapi.com/search',
-//   params: {q: 'chicken'},
-//   headers: {
-//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-//     'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-//   }
-// };
+const options = {
+  method: 'GET',
+  url: 'https://edamam-recipe-search.p.rapidapi.com/search',
+  params: {q: 'chicken'},
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+  }
+};
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
