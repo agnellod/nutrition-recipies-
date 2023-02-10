648e9a59b8e53175516616b308ec9f65faf08635
Matt Moore <mattmoore2014@gmail.com>
2023-02-09 21:08:27 -0700

    comments

--- Develop/front.js
+++ Develop/front.js
@@ -1,5 +1,18 @@
 const searchButtonEl = document.querySelector('.is-info');
 
+  // Use Array.forEach to add an event listener to each checkbox.
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      enabledSettings = 
+        Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+        .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+        .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+    
+      console.log(enabledSettings)
+    })
+  });
+  
+
 function switchToSecondPage(event) {
     event.preventDefault();
     
--- Develop/script.js
+++ Develop/script.js
@@ -9,17 +9,6 @@ var id = 'eceea0f0';
 var url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${id}&app_key=${apiKey}`;
 const options = {method: 'GET'};
   
-  // Use Array.forEach to add an event listener to each checkbox.
-checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('change', function() {
-    enabledSettings = 
-      Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
-      .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
-      .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
-  
-    console.log(enabledSettings)
-  })
-});
 
 
 
