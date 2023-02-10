b56a9959a86779adc70826f5ab21d775d028710f
Dylan <agnello.dylan95@gmail.com>
2023-02-09 18:21:09 -0700

    adding different api to front js

--- Develop/front.js
+++ Develop/front.js
@@ -1,26 +1,16 @@
-// var random = document.getElementById("display-recipe");
-// const apiKey = '63fb7278f9msh105fbe7a01b5ab6p11a57bjsnfdf002c44f30'
-// var randomRecipe = function(){
-//     var frontApi = 'https://tasty.p.rapidapi.com/recipes/auto-complete?q=${display}&appid=${apiKey}prefix=chicken%20soup'
-//         fetch(frontApi)
-//         .then(function (response) {
-//             return response.json()
-//         })
-//         .then(function(data){
-//             console.log(data)
-//             random.append(response);
-//         })
+var random = document.getElementById("display-recipe");
+const apiKey = '1'
+var randomRecipe = function(){
+    var frontApi = 'http://www.themealdb.com/api/json/v1/1/random.php';
+            fetch(frontApi)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+            
+        })
     
-// }
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '63fb7278f9msh105fbe7a01b5ab6p11a57bjsnfdf002c44f30',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
-};
-
-fetch('https://tasty.p.rapidapi.com/tags/list', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+}
+randomRecipe();
+;
