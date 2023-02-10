ab1ec84b4a4e2664368a3988a4baae8e213b895c
Matt Moore <mattmoore2014@gmail.com>
2023-02-10 15:18:10 -0700

    .

--- Develop/front.js
+++ Develop/front.js
@@ -11,7 +11,26 @@ function switchToSecondPage(event) {
         return;
     }
 
-
+    // document.getElementById('dropdown-item', '.vegan').addEventListener('click', function (check) {
+    //     check.preventDefault();
+    //     let checkedBox = document.querySelector('.checkbox');
+    //     let checkedOutput = checkedBox.append(queryString);
+        
+    //     for (let check of checkedBox) {
+    //         const d = history.healthlabels[5];
+    //         fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${id}&app_key=${apiKey}&health=vegan`)
+            
+    //         .then((r)=> r.json())
+            
+    //         .then( d => {
+                
+    //             let checkedPreference = document.textContent(check + history.healthlabels[0])
+    //             console.log(checkedPreference); // confirm that these query calls link to our api specifically
+    //             checkedOutput.appendChild(checkedPreference)
+    //         }) 
+    //     }
+            
+    //     })
     const queryString = './secondpage.html?q=' + searchInputVal + '&dietaryrestriction=' + dietaryRestVal;
 
     // location.assign(queryString);
