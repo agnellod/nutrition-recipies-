2f61768f3aaa883fa391f5cc0715b364532242f3
Matt Moore <mattmoore2014@gmail.com>
2023-02-14 11:23:23 -0700

    .

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,11 @@ healthStr && `&health=${urlSplitHealth}`;
 var search =document.getElementById("#box-1");
 var url = `https://api.edamam.com/api/recipes/v2?type=public&q=${urlSplitq}&app_id=eceea0f0&app_key=356844f53ca5f3e2604e67318c228565${healthStr}`;
 
+
+for (let i = 0; i < 10; i++) {
+   
+  
+
 fetch(url)
 .then(function (response) {
     return response.json()
@@ -152,4 +157,4 @@ var randomRecipe = function(){
 
 
 
-
+    } 
